ececa7c2112effbd418f84723a69185dc19ded8c
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-15 00:23:48 +0500

    Update urls.py

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from restaurant import views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('menu/', views.menu_view, name='menu'),
-    path('booking/', views.booking_view, name='booking'),
-    path('waiter/', views.waiter_hall, name='waiter'),
-    path('kitchen/', views.kitchen_view, name='kitchen'),
-    path('reports/', views.reports_view, name='reports'),
-    path('api/', include('restaurant.urls')),
+    path('', include('restaurant.urls')),
 ]
