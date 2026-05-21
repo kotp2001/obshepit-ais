3236e1020ff8ae98bbeadd69e0f291a001bfb98b
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-21 21:23:27 +0500

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -149,3 +149,21 @@ class MaintenanceLog(models.Model):
     
     def __str__(self):
         return f'{self.date} - {self.work_performed[:50]}'
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Администратор'),
+        ('waiter', 'Официант'),
+        ('chef', 'Повар'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='waiter', verbose_name="Роль")
+    pin_code = models.CharField(max_length=4, blank=True, null=True, verbose_name="Пин-код")
+    
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
