f41b6f68798dfff718772f2c07fb78b930b06901
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-21 19:39:29 +0500

    Update settings.py

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-8x9y2z1w0v7u6t5s4r3q2p1o0n9m8l7k6j5i4h3g2f1d0s9a8z7x6c5v4b3n2m1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8x9y2z1w0v7u6t5s4r3q2p1o0n9m8l7k6j5i4h3g2f1d0s9a8z7x6c5v4b3n2m1')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
@@ -75,6 +76,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+# Кастомная админка
+ADMIN_SITE_HEADER = "АИС Общепит"
+ADMIN_SITE_TITLE = "АИС Общепит - Панель администратора"
+ADMIN_INDEX_TITLE = "Управление системой ресторана"
+
+# Перенаправление после логина
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/admin-panel/'
 LOGOUT_REDIRECT_URL = '/'
