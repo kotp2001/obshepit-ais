5c6ab2d364649a90b88c2420b2eff8e20e8ff910
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-23 13:12:18 +0500

    Update urls.py

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/backup/', include('restaurant.backup_urls')),
     path('', include('restaurant.urls')),
 ]
