55580e8757d70a5ab20954d0669fda17ed3b7925
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-23 17:20:25 +0500

    Update backup_urls.py

--- restaurant/backup_urls.py
+++ restaurant/backup_urls.py
@@ -4,12 +4,8 @@ from . import views
 app_name = 'backup'
 
 urlpatterns = [
-    # Страница со списком бэкапов и формой создания нового
     path('', views.admin_backup, name='index'),
-    # Скачивание файла бэкапа
     path('download/<str:filename>/', views.admin_backup_download, name='download'),
-    # Восстановление базы данных из бэкапа
     path('restore/<str:filename>/', views.admin_backup_restore, name='restore'),
-    # Удаление файла бэкапа
     path('delete/<str:filename>/', views.admin_backup_delete, name='delete'),
 ]
