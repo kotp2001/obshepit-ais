6c6362746a1cbeb542b3d42786cbbf681ff14c40
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-23 17:48:44 +0500

    Update backup_urls.py

--- restaurant/backup_urls.py
+++ restaurant/backup_urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-from . import views
-
-app_name = 'backup'
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Страница со списком бэкапов и формой создания нового
+    path('admin/', admin.site.urls),
+    # Подключаем URL-адреса резервного копирования из приложения restaurant
+    path('admin/backup/', include('restaurant.backup_urls')),
+    path('', include('restaurant.urls')),
+     # Страница со списком бэкапов и формой создания нового
     path('', views.admin_backup, name='index'),
     # Скачивание файла бэкапа
     path('download/<str:filename>/', views.admin_backup_download, name='download'),
