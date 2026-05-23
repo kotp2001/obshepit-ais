351050543807a5b72caf9fe0749e0374990ab666
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-23 16:33:16 +0500

    Update settings.py

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -62,9 +62,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'ru-ru'
-TIME_ZONE = 'Asia/Yekaterinburg'
+TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_TZ = True
+USE_TZ = False
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
